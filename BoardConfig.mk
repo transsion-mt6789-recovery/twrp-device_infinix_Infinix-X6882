a12913f2939ff2d03f0298f459dc8346928662ac
Rama Bondan Prakoso <ramanarubp@gmail.com>
2024-11-27 15:42:53 +0700

    X6882: Initial TWRP device tree
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/infinix/Infinix-X6882
+
+# Inherit from mt6789-common
+include device/transsion/mt6789-common/BoardConfigCommon.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := Infinix-X6882
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_Infinix-X6882
+TARGET_RECOVERY_DEVICE_MODULES := libinit_Infinix-X6882
+
+# TWRP Configs
+TW_DEVICE_VERSION := X6882_by_rama982
---
+++ README.md
@@ -0,0 +1,1 @@
+## Device config for TWRP Infinix HOT 50 ( Infinix-X6882 )
